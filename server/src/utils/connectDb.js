179d892cc5e6cbf0aeaeb85f4da78945264a7fee
Chitvan Garg <gargchitvan378@gmail.com>
2024-11-11 14:58:12 +0530

    db config

--- server/src/utils/connectDb.js
+++ server/src/utils/connectDb.js
@@ -2,7 +2,11 @@ import mongoose from "mongoose";
 
 export const connectDb = async () => {
   try {
-    await mongoose.connect(process.env.CONNECTION_STRING);
+    await mongoose.connect(process.env.CONNECTION_STRING, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 5000
+    });
     return "Connected to DB";
   } catch (error) {
     throw new Error(`Failed to connect to DB: ${error}`);
